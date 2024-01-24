54eabfc357b2d488dc5aececd772264d0fb1ae9b
render41 <gabrielservice05@gmail.com>
2024-01-24 15:16:23 -0300

    Create Project

---
+++ app/src/androidTest/java/com/arcastudio/project01_travelconsumption/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.arcastudio.project01_travelconsumption
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.arcastudio.project01_travelconsumption", appContext.packageName)
+    }
+}
