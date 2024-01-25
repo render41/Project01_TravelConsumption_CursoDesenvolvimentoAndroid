ebb44529b8209bfa67580e2c5538783f8332bf9c
render41 <gabrielservice05@gmail.com>
2024-01-25 12:47:11 -0300

    Finish Layout

--- app/src/main/java/com/arcastudio/project01_travelconsumption/MainActivity.kt
+++ app/src/main/java/com/arcastudio/project01_travelconsumption/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.arcastudio.project01_travelconsumption
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        modifyColorStatusBar()
         setContentView(R.layout.activity_main)
+    }
+
+    private fun modifyColorStatusBar() {
+        window.statusBarColor = ContextCompat.getColor(this, R.color.green_base)
     }
 }
